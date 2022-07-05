004d60ecdec890eb141ab5e32a7753532bee999e
joserb10 <joserb10>
2022-07-05 15:49:09 -0500

    modificar end point /products para obtener todos los productos si no se envia el parametro category

--- src/main/java/com/tiendaonline/main/controllers/ProductController.java
+++ src/main/java/com/tiendaonline/main/controllers/ProductController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //Api Rest
@@ -21,11 +23,20 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    //Allow CrossOrigin a todos los clientes
+    @CrossOrigin(origins = "*")
     //Petición Get con ruta base /products
     @GetMapping
     public ResponseEntity<List<Product>> getAllProductsByCategory(@Param("category") Integer category) {
-        //Obtener todos los productos desde la base de datos
-        List<Product> products = productService.findAllByCategory(category);
+        List<Product> products = new ArrayList<>();
+        //Validar que se envíe el parametro
+        if (category != null) {
+            //Obtener todos los productos desde la base de datos
+            products = productService.findAllByCategory(category);
+        } else {
+            //Obtener todos los productos desde la base de datos
+            products = productService.findAll();
+        }
 
         //Retornar un ResponseEnitity con los productos y el HttpStatus 200 al cliente
         return new ResponseEntity<List<Product>>(products,null, HttpStatus.OK);
