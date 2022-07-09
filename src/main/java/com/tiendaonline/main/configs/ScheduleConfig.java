ace906b446698b75207f3deb2085dc5ac45b86b9
joserb10 <joserb10>
2022-07-09 16:59:48 -0500

    Variacion tiempo ejecucion tarea programada 4250ms

--- src/main/java/com/tiendaonline/main/configs/ScheduleConfig.java
+++ src/main/java/com/tiendaonline/main/configs/ScheduleConfig.java
@@ -3,6 +3,7 @@ package com.tiendaonline.main.configs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/*Configuración para habilitar tareas programadas en el proyecto*/
 @Configuration
 @EnableScheduling
 public class ScheduleConfig {
--- src/main/java/com/tiendaonline/main/crons/RequestScheduler.java
+++ src/main/java/com/tiendaonline/main/crons/RequestScheduler.java
@@ -1,17 +1,25 @@
 package com.tiendaonline.main.crons;
 
+import com.tiendaonline.main.entities.Category;
 import com.tiendaonline.main.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class RequestScheduler {
+    /*Inyectar dependencia del service de categorias*/
     @Autowired
     private CategoryService categoryService;
 
-    @Scheduled(cron = "*/5 * * * * *")
-    public void requestDBCategory() {
-        categoryService.findAll();
+    /*Tarea programada para ejecutar una peticion a la base de datos cada 4250 milisegundos*/
+    @Async
+    @Scheduled(fixedRate = 4250)
+    public List<Category> requestDBCategory() {
+        //Devolver todas las categorias
+        return categoryService.findAll();
     }
 }
